d327c63b9c80dde0aff1fd3b5f28a56a1653afda
Puentebravo <puentematos@gmail.com>
2021-06-01 18:17:10 -0400

    Coded out card footer component for user feedback

---
+++ src/components/passcardfooter.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function PassCardFooter() {
+    return (
+        <div className="card-footer">
+        <p id="spcCharEl"></p>
+        <p id="lCaseEl"></p>
+        <p id="uCaseEl"></p>
+        <p id="numCharEl"></p>
+        <button id="generate" className="btn">GENERATE</button>
+      </div>
+    )
+}
+
+export default PassCardFooter;
