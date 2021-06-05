f67fb4d2216130fedccf5ede54d6abf442b8299a
Puentebravo <puentematos@gmail.com>
2021-06-04 21:47:19 -0400

    Added imports, laid out components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Lead from './components/lead';
 import PassCard from './components/passcard';
 
 function App() {
   return (
+    <>
+    <Lead />
     <PassCard />
+    </>
   );
 }
 
