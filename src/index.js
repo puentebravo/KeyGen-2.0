39bd30b64afb8bf1da2824fdbadda1588b3697cd
Puentebravo <puentematos@gmail.com>
2021-06-21 18:40:54 -0400

    Removed css import from index, disabled bootstrap

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.bundle";
 
 ReactDOM.render(
   <React.StrictMode>
