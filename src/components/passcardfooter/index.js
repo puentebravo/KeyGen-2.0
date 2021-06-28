811c87ec68536ae182c088b21d75634c5bade10a
Puentebravo <puentematos@gmail.com>
2021-06-28 19:00:11 -0400

    Added toggle switch HTML

--- src/components/passcardfooter/index.js
+++ src/components/passcardfooter/index.js
@@ -1,17 +1,33 @@
 import React from "react";
 import GenerateBtn from "../generatebtn/index";
-import "./passcardfooter.css"
+import "./passcardfooter.css";
 
 function PassCardFooter() {
-    return (
-        <div className="card-footer">
-        <p id="spcCharEl"></p>
+  return (
+    <div className="card-footer">
+      {/* <p id="spcCharEl"></p>
         <p id="lCaseEl"></p>
         <p id="uCaseEl"></p>
-        <p id="numCharEl"></p>
-        <GenerateBtn />
-      </div>
-    )
+        <p id="numCharEl"></p> */}
+      <label className="switch" id="spcCharEl">
+        <input type="checkbox" />
+        <span className="slider"></span>
+      </label>
+      <label className="switch" id="lCaseEl">
+        <input type="checkbox" />
+        <span className="slider"></span>
+      </label>
+      <label className="switch" id="uCaseEl">
+        <input type="checkbox" />
+        <span className="slider"></span>
+      </label>
+      <label className="switch" id="numCharEl">
+        <input type="checkbox" />
+        <span className="slider"></span>
+      </label>
+      <GenerateBtn />
+    </div>
+  );
 }
 
 export default PassCardFooter;
