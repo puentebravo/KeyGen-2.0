bae9d1c7ec485827d41cf59cc163a4bf74b3e47e
Puentebravo <puentematos@gmail.com>
2021-06-28 20:09:27 -0400

    Switched styling to bootstrap, added row to center on grid

--- src/components/generatebtn/index.js
+++ src/components/generatebtn/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import "./generatebtn.css"
 
 function GenerateBtn() {
   return (
-    <button id="generate" className="btn">
+    <div className="row justify-content-center">
+    <button id="generate" className="btn btn-success text-center">
       GENERATE
     </button>
+    </div>
   );
 }
 
