a5bab7ff09c96d24ba5688ae8d4b9123b2c2d7c3
Puentebravo <puentematos@gmail.com>
2021-06-28 20:10:44 -0400

    Deleted separate css sheet, added toggle switches

--- src/components/passcardfooter/index.js
+++ src/components/passcardfooter/index.js
@@ -1,30 +1,53 @@
 import React from "react";
 import GenerateBtn from "../generatebtn/index";
-import "./passcardfooter.css";
 
 function PassCardFooter() {
   return (
     <div className="card-footer">
       {/* <p id="spcCharEl"></p>
-        <p id="lCaseEl"></p>
-        <p id="uCaseEl"></p>
-        <p id="numCharEl"></p> */}
-      <label className="switch" id="spcCharEl">
-        <input type="checkbox" />
-        <span className="slider"></span>
-      </label>
-      <label className="switch" id="lCaseEl">
-        <input type="checkbox" />
-        <span className="slider"></span>
-      </label>
-      <label className="switch" id="uCaseEl">
-        <input type="checkbox" />
-        <span className="slider"></span>
-      </label>
-      <label className="switch" id="numCharEl">
-        <input type="checkbox" />
-        <span className="slider"></span>
-      </label>
+      <p id="lCaseEl"></p>
+      <p id="uCaseEl"></p>
+      <p id="numCharEl"></p> */}
+      <div class="form-check form-switch" id="spcCharEl">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          id="flexSwitchCheckDefault"
+        />
+        <label class="form-check-label" for="flexSwitchCheckDefault">
+          Special Characters
+        </label>
+      </div>
+      <div class="form-check form-switch" id="lCaseEl">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          id="flexSwitchCheckDefault"
+        />
+        <label class="form-check-label" for="flexSwitchCheckDefault">
+          Lower Case Letters
+        </label>
+      </div>
+      <div class="form-check form-switch" id="uCaseEl">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          id="flexSwitchCheckDefault"
+        />
+        <label class="form-check-label" for="flexSwitchCheckDefault">
+          Upper Case Letters
+        </label>
+      </div>
+      <div class="form-check form-switch" id="numCharEl">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          id="flexSwitchCheckDefault"
+        />
+        <label class="form-check-label" for="flexSwitchCheckDefault">
+          Numbers
+        </label>
+      </div>
       <GenerateBtn />
     </div>
   );
