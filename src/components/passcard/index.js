9e0556ac5175aedbd96408e52efb8d2b1bfd6091
Puentebravo <puentematos@gmail.com>
2021-07-05 22:14:14 -0400

    added default value to switches

--- src/components/passcard/index.js
+++ src/components/passcard/index.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PassCardFooter from "../passcardfooter/index";
 import PassOutput from "../passwordoutput/index";
-import "./passcard.css"
+import "./passcard.css";
 
 function PassCard() {
+  const [password, setPassword] = useState([]);
+
   return (
     <div className="card">
       <div className="card-header">
         <h2 className="text-center">Generate Password</h2>
       </div>
-      <PassOutput />
-      <PassCardFooter />
+      <PassOutput password={password} />
+      <PassCardFooter password={password} />
     </div>
   );
 }
