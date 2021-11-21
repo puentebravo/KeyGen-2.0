4c77b1db6839198c73d3a305849c9b6cb3c202f8
Puentebravo <puentematos@gmail.com>
2021-11-21 16:56:17 -0500

    updated component to track its own state

--- src/components/generatebtn/index.js
+++ src/components/generatebtn/index.js
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 function GenerateBtn(props) {
+  const generatePass = props.generatePass;
 
-  const generatePass = props.generatePass
+  const [pwLength, setpwLength] = useState()
+
+  // need to figure out a way to grab pw length from the form element
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    generatePass(pwLength)
+  };
+
+  const handleInputChange = (event) => {
+    console.log(event.target.value);
+    setpwLength(event.target.value)
+  }
+
+
 
   return (
     <div className="row justify-content-center">
-    <button id="generate" className="btn btn-success text-center" onClick={generatePass}>
-      GENERATE
-    </button>
+        <div className="col-auto">
+          <p className="lead">Password Length: </p>
+        </div>
+        <div className="col-auto">
+          <label for="pwLength" className="visually-hidden">
+            Password
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="inputLength"
+            placeholder=""
+            name="pwLength"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="col-auto">
+          <button
+            id="generate"
+            className="btn btn-success text-center"
+            type="submit"
+            onSubmit={handleFormSubmit}
+            onClick={handleFormSubmit}
+          >
+            GENERATE
+          </button>
+        </div>
     </div>
   );
 }
