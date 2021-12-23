d85ba5e74ac966d0b26de58604c94c102c95d626
Puentebravo <puentematos@gmail.com>
2021-12-22 23:32:27 -0500

    re-formatted index.js for better readability

--- src/components/passcard/index.js
+++ src/components/passcard/index.js
@@ -5,27 +5,24 @@ import "./passcard.css";
 
 function PassCard() {
   const [password, setPassword] = useState();
-  
 
   const generatePass = (pwLength) => {
-    
-    let numLength = parseInt(pwLength)
-    
-    let pwSource = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.!@#$%^&*.,?";
+    let numLength = parseInt(pwLength);
+
+    let pwSource =
+      "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.!@#$%^&*.,?";
     let genPass = "";
 
-    
-
     if (numLength >= 8 && numLength <= 128) {
-
       console.log(pwSource);
       for (let i = 0, x = pwSource.length; i < numLength; i++) {
         genPass += pwSource.charAt(Math.floor(Math.random() * x));
       }
-      console.log(genPass);
-      setPassword(genPass)
+      setPassword(genPass);
     } else {
-      setPassword("You require more pylons! Please select a number between 8 and 128.")
+      setPassword(
+        "You require more pylons! Please select a number between 8 and 128."
+      );
     }
   };
 
