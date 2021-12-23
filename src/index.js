9ee4acf70f13e329fe9a89695513f4649460804d
Puentebravo <puentematos@gmail.com>
2021-12-23 00:04:34 -0500

    removed strict mode from index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,6 @@ import * as serviceWorkerRegistration from "./serviceWorkerRegistration.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorkerRegistration.register();
