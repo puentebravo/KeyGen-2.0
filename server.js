3a9e42baf06a3994552cbcac29a654c1198166cb
Puentebravo <puentematos@gmail.com>
2021-12-23 00:41:15 -0500

    Added express backend

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, 'Client/build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'Client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on ${PORT}`)
+});
