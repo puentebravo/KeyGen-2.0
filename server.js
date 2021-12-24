8a9f088357437e78daebca02da0e68b749b8a87b
Puentebravo <puentematos@gmail.com>
2021-12-23 23:08:40 -0500

    fixed syntax error in main route

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("./client/public"));
 }
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./Client/build/index.html"));
 });
 
 app.listen(PORT, () => {
