6a45e8c7887fcdd9745e7eb0c455d056bd2de023
Puentebravo <puentematos@gmail.com>
2021-12-23 23:34:57 -0500

    fixed URIs

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("./Client/public"));
+  app.use(express.static("./client/public"));
 }
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./Client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, () => {
-    console.log(`App listening on ${PORT}`)
+  console.log(`App listening on ${PORT}`);
 });
