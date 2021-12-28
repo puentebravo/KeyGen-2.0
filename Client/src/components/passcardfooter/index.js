ae8fab0876a20a7218e2fcb12637c4f8be861c89
Puentebravo <puentematos@gmail.com>
2021-12-28 17:42:06 -0500

    formatted for readability

--- Client/src/components/passcardfooter/index.js
+++ Client/src/components/passcardfooter/index.js
@@ -4,7 +4,12 @@ import GenerateBtn from "../generatebtn/index";
 function PassCardFooter(props) {
   return (
     <div className="card-footer">
-      <GenerateBtn generatePass={props.generatePass} />
+      <GenerateBtn
+        generatePass={props.generatePass}
+        password={props.password}
+        copyPass={props.copyPass}
+        copyStatus={props.copyStatus}
+      />
     </div>
   );
 }
