e7a65eb356a4596fedcd81beed9b28d9159d994d
Puentebravo <puentematos@gmail.com>
2022-01-26 20:34:43 -0500

    re-added header

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,12 +1,12 @@
-import Lead from './components/lead/index';
-import PassCard from './components/passcard/index';
-import './App.css';
+import Lead from "./components/lead/index";
+import PassCard from "./components/passcard/index";
+import "./App.css";
 
 function App() {
   return (
     <div>
-    <Lead />
-    <PassCard />
+      <Lead />
+      <PassCard />
     </div>
   );
 }
